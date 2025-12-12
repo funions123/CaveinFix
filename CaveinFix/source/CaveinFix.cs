64fe19207469ed9ffdf92d2419a91ef278381ad4
funions123 <39248127+funions123@users.noreply.github.com>
2025-12-12 02:03:29 -0500

    Add project files.

---
+++ CaveinFix/source/CaveinFix.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using Vintagestory;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent; 
+using Vintagestory.ServerMods;  
+
+namespace CaveinFix
+{
+    public class CaveinFixModSystem : ModSystem
+    {
+        private ICoreServerAPI _sapi;
+
+        private Dictionary<int, int> rockToHardenedMap = new Dictionary<int, int>();
+
+        public override bool ShouldLoad(EnumAppSide side)
+        {
+            return side == EnumAppSide.Server;
+        }
+
+        public override double ExecuteOrder()
+        {
+            return 0.2;
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            _sapi = api;
+
+            api.Event.ChunkColumnGeneration(OnChunkColumnGen, EnumWorldGenPass.TerrainFeatures, "standard");
+        }
+
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            foreach (Block block in api.World.Blocks)
+            {
+                if (block.Code == null) continue;
+
+                Console.WriteLine(block.Code);
+
+                if ((block.Code.Path.StartsWith("rock-") || block.Code.Path.StartsWith("crackedrock-")) && block.HasBehavior<BlockBehaviorUnstableRock>())
+                {
+                    string rockType = block.Variant["rock"];
+
+                    Console.WriteLine(rockType);
+
+                    Block hardenedBlock = api.World.GetBlock(new AssetLocation("caveinfix", "hardenedrock-" + rockType));
+
+                    if (hardenedBlock != null)
+                    {
+                        Console.WriteLine("Dictionary success");
+                        rockToHardenedMap[block.Id] = hardenedBlock.Id;
+                    }
+                }
+            }
+        }
+
+        private void OnChunkColumnGen(IChunkColumnGenerateRequest request)
+        {
+            var chunks = request.Chunks;
+
+            int seaLevel = TerraGenConfig.seaLevel - 1;
+
+            const int chunksize = GlobalConstants.ChunkSize;
+            const int chunksizeSquared = chunksize * chunksize;
+
+            int yMax = chunks[0].MapChunk.YMax;
+            int cyMax = Math.Min(yMax / chunksize + 1, _sapi.WorldManager.MapSizeY / chunksize);
+
+            for (int cy = 0; cy < cyMax; cy++)
+            {
+                IChunkBlocks chunkdata = chunks[cy].Data;
+                int yStart = cy == 0 ? 1 : 0;
+                int yEnd = chunksize - 1;
+
+                for (int baseindex3d = 0; baseindex3d < chunksizeSquared; baseindex3d++)
+                {
+                    int blockIdBelow;
+                    int index3d = baseindex3d + (yStart - 1) * chunksizeSquared;
+
+                    if (yStart == 0)
+                    {
+                        if (cy > 0)
+                        {
+                            blockIdBelow = chunks[cy - 1].Data.GetBlockIdUnsafe(index3d + chunksize * chunksizeSquared);
+                        }
+                        else
+                        {
+                            blockIdBelow = 0;
+                        }
+                    }
+                    else
+                    {
+                        blockIdBelow = chunkdata.GetBlockIdUnsafe(index3d);
+                    }
+
+                    for (int y = yStart; y <= yEnd; y++)
+                    {
+                        index3d += chunksizeSquared;
+                        int currentBlockId = chunkdata.GetBlockIdUnsafe(index3d);
+
+                        if (blockIdBelow == 0 && currentBlockId != 0)
+                        {
+                            if (rockToHardenedMap.TryGetValue(currentBlockId, out int hardenedId))
+                            {
+                                chunkdata.SetBlockUnsafe(index3d, hardenedId);
+                                currentBlockId = hardenedId;
+                            }
+                        }
+
+                        blockIdBelow = currentBlockId;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CaveinFix/source/Patches/Patches.cs
@@ -0,0 +1,102 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+namespace CaveinFix.Patches;
+
+public class CaveinFixPatches : ModSystem
+{
+    private Harmony _patcher;
+
+    public static ICoreServerAPI _api;
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        _api = api;
+
+        _patcher = new Harmony(Mod.Info.ModID);
+        _patcher.PatchAll();
+    }
+
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        if (api.Side != EnumAppSide.Server)
+        {
+            return;
+        }
+    }
+
+    public override void Dispose()
+    {
+        _patcher?.UnpatchAll(Mod.Info.ModID);
+        base.Dispose();
+    }
+}
+
+internal static class Patches
+{
+    [HarmonyPatch(typeof(BlockBehaviorUnstableRock))]
+    public static class UnstableRockPatch
+    {
+        [HarmonyPatch("searchCollapsible")]
+        [HarmonyPostfix]
+        public static void SearchCollapsiblePostfix(
+            BlockBehaviorUnstableRock __instance,
+            BlockPos startPos,
+            ICoreAPI ___api,
+            ref CollapsibleSearchResult __result
+        )
+        {
+            BlockPos belowPos = startPos.DownCopy();
+            Block blockBelow = ___api.World.BlockAccessor.GetBlock(belowPos, BlockLayersAccess.Solid);
+
+            if (blockBelow.BlockId != 0 && blockBelow.SideIsSolid(___api.World.BlockAccessor, belowPos, BlockFacing.UP.Index))
+            {
+                bool foundVerticalSupport = false;
+                double stabilityBelow = 0;
+
+                if (blockBelow.HasBehavior<BlockBehaviorUnstableRock>())
+                {
+                    stabilityBelow = blockBelow.GetBehavior<BlockBehaviorUnstableRock>().getInstability(belowPos);
+
+                    if (stabilityBelow < 1.0) foundVerticalSupport = true;
+                }
+                else
+                {
+                    foundVerticalSupport = true;
+                    stabilityBelow = 0;
+                }
+
+                if (foundVerticalSupport)
+                {
+                    __result.Instability = Math.Min(__result.Instability, (float)stabilityBelow);
+
+                    __result.Unconnected = false;
+                }
+            }
+        }
+
+        [HarmonyPatch("getInstability")]
+        [HarmonyPrefix]
+        public static bool GetInstabilityPrefix(
+            BlockBehaviorUnstableRock __instance,
+            BlockPos pos,
+            ref double __result
+        )
+        {
+            var method = AccessTools.Method(typeof(BlockBehaviorUnstableRock), "searchCollapsible");
+            var res = (CollapsibleSearchResult)method.Invoke(__instance, new object[] { pos, false });
+
+            __result = res.Instability;
+
+            return false;
+        }
+    }
+}
